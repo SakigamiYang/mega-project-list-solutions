8926f35fa940c479ed13d4ef17de1152e3306cf5
Sakigami Yang <sakigami@163.com>
2019-11-02 22:43:50 +0800

    add classalgorithm.CollatzConjecture and test

---
+++ src/main/java/me/sakigamiyang/exercise/classalgorithm/CollatzConjecture.java
@@ -0,0 +1,40 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Zhixiao Yang
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.sakigamiyang.exercise.classalgorithm;
+
+class CollatzConjecture {
+    static int getSteps(int n) throws IllegalArgumentException {
+        if (n <= 1) {
+            throw new IllegalArgumentException("n must be greater than 1.");
+        }
+
+        int steps = 0;
+        while (n != 1) {
+            n = (n % 2 == 0) ? (n / 2) : (3 * n + 1);
+            steps += 1;
+        }
+        return steps;
+    }
+}
