2c3ea4f5ca4ea8b5186be911dc967560e0b72de5
SakigamiYang <sakigami@163.com>
2019-11-06 13:59:06 +0800

    change assert method

--- src/test/java/me/sakigamiyang/exercise/text/CheckIfPalindromeTest.java
+++ src/test/java/me/sakigamiyang/exercise/text/CheckIfPalindromeTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class CheckIfPalindromeTest {
     @Test
     public void testGetFizzBuzz() {
-        Assert.assertEquals(true, CheckIfPalindrome.check(""));
-        Assert.assertEquals(true, CheckIfPalindrome.check("a"));
-        Assert.assertEquals(true, CheckIfPalindrome.check("bb"));
-        Assert.assertEquals(false, CheckIfPalindrome.check("cd"));
-        Assert.assertEquals(true, CheckIfPalindrome.check("aba"));
-        Assert.assertEquals(false, CheckIfPalindrome.check("uab"));
-        Assert.assertEquals(true, CheckIfPalindrome.check("racecar"));
-        Assert.assertEquals(true, CheckIfPalindrome.check("racdcar"));
-        Assert.assertEquals(false, CheckIfPalindrome.check("racedar"));
-        Assert.assertEquals(false, CheckIfPalindrome.check("racecbr"));
-        Assert.assertEquals(false, CheckIfPalindrome.check("racecas"));
+        Assert.assertTrue(CheckIfPalindrome.check(""));
+        Assert.assertTrue(CheckIfPalindrome.check("a"));
+        Assert.assertTrue(CheckIfPalindrome.check("bb"));
+        Assert.assertFalse(CheckIfPalindrome.check("cd"));
+        Assert.assertTrue(CheckIfPalindrome.check("aba"));
+        Assert.assertFalse(CheckIfPalindrome.check("uab"));
+        Assert.assertTrue(CheckIfPalindrome.check("racecar"));
+        Assert.assertTrue(CheckIfPalindrome.check("racdcar"));
+        Assert.assertFalse(CheckIfPalindrome.check("racedar"));
+        Assert.assertFalse(CheckIfPalindrome.check("racecbr"));
+        Assert.assertFalse(CheckIfPalindrome.check("racecas"));
     }
 }
